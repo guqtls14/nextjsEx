f7fa6c96c89c6ee676a6ca2f6966b1da37d233aa
kyle-shk <guqtls85@naver.com>
2022-09-13 22:01:30 +0900

    feat: detail page 컴포넌트 구성(props전달)

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,7 +1,32 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const item = getEventById(eventId);
+
+  if (!item) {
+    return <p>No event found!</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={item.title} />
+      <EventLogistics
+        date={item.date}
+        address={item.location}
+        image={item.image}
+        imageAlt={item.title}
+      />
+      <EventContent>
+        <p>{item.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
